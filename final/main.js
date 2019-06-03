ce6ec223cc91eef86feb655c26259db34ff51bfb
dominguez-rosie <47767513+dominguez-rosie@users.noreply.github.com>
2019-06-03 15:26:22 -0700

    aded badge count

--- final/main.js
+++ final/main.js
@@ -1,5 +1,18 @@
-$(document).ready(function{
-	cart =[];
+$(document).ready(function() {
+	
+
+
+	$(".add").on("click", function() {
+		$(".cart").removeClass('disabled');
+		let currentTotal = $(".badge-info").text();
+		$(".badge-info").text(+currentTotal + 1);
+	});
+
+	$('.js-modal-popper').click(function(){
+		$('.modal').modal({
+			backdrop: 'static'	
+		})
+	});
 
 
 });
